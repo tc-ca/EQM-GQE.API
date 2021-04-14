96b22a05c50b90a0d51adb1c4885c9bb46ba4858
Jason Au <jason@infusionws.com>
2021-04-14 10:53:35 -0400

    Initial commit

---
+++ Controllers/QuestionnaireController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EQM_GQE.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuestionnaireController : ControllerBase
+    {
+        private static readonly string[] Templates = new[]
+        {
+            "Questionnaire Template 1", "Questionnaire Template 2", "Questionnaire Template 3", "Questionnaire Template 4", "Questionnaire Template 5", "Questionnaire Template 6"
+        };
+
+        private readonly ILogger<QuestionnaireController> _logger;
+
+        public QuestionnaireController(ILogger<QuestionnaireController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Questionnaire> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Questionnaire
+            {
+                Id = Guid.NewGuid(),
+                DateCreated = DateTime.Now.AddDays(index),
+                Template = Templates[rng.Next(Templates.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
