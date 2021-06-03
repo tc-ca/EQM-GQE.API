3aa54cbb93ae18d38c032b487d56d0297b8d79f7
Rajib Bosh <rajib.bosh@tc.gc.ca>
2021-06-03 11:55:17 -0400

    Created Model under resources folder.

---
+++ EQM-GQE.SHARED-RESOURCES/Models/DocumentType.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace EQM_GQE.SHARED_RESOURCES.Models
+{
+    public class DocumentType
+    {
+        public long Id { get; set; }
+        
+        public string DocumentType_EN { get; set; }
+
+        public string DocumentType_FR { get; set; }
+
+    }
+}
---
+++ EQM-GQE.SHARED-RESOURCES/Models/SecurityClassification.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace EQM_GQE.SHARED_RESOURCES.Models
+{
+    public class SecurityClassification
+    {
+        public long Id { get; set; }
+        
+        public string SecurityClassification_EN { get; set; }
+
+        public string SecurityClassification_FR { get; set; }
+
+    }
+}
