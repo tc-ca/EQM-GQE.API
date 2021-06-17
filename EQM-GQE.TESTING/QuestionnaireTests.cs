e1b233102b4247f32ea9e63ca162e1acc81ff985
Rajib Bosh <rajib.bosh@tc.gc.ca>
2021-06-17 15:50:53 -0400

    Working with Stephen on Moq/XUnit unit testing.

--- EQM-GQE.TESTING/QuestionnaireTests.cs
+++ EQM-GQE.TESTING/QuestionnaireTests.cs
@@ -1,6 +1,9 @@
 using EQM_GQE.API.Controllers;
 using EQM_GQE.SHARED_RESOURCES.Interfaces;
+using EQM_GQE.SHARED_RESOURCES.Models;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
+using System.Threading.Tasks;
 using Xunit;
 
 
@@ -10,13 +13,43 @@ namespace EQM_GQE.TESTING
     public class QuestionnaireTests
     {
         [Fact]
-        public void QuestionnaireLogicValidator()
+        public async Task QuestionnaireLogicValidator()
         {
-           // var questionnaireController = new QuestionnaireController();
+            // Arange
+            Questionnaire questionnaire = new()
+            {
+                Template = "Moq Test",
+                DocumentTitle = "Moq Test",
+                BusinessLineId = 1,
+                DocumentTypeId = 1,
+                DocumentStatusId = 1,
+                SecurityClassificationId = 1,
+                CreatedOn = System.DateTime.Now,
+                ModifiedOn = System.DateTime.Now,
+                CreatedBy = "Moq Test User",
+                ModifiedBy = "Moq Test User",
+                ActiveStatus = true,
+                DocumentVersion = 1,
+                EffectiveDate = System.DateTime.Now,
+                ChangeSummary = "Moq Test Summary",
+                OrganisationAccessibility = true,
+                ParentId = 0
+            };
 
-           // Mock<IQuestionnaireLogic> postQuestionnaireLogicValidatior = new Mock<IQuestionnaireLogic>();
+            //Mock<IQuestionnaireRepository> postQuestionnaireRepositoryValidatior = new Mock<IQuestionnaireRepository>();
+            Mock<IQuestionnaireLogic> postQuestionnaireLogicValidatior = new Mock<IQuestionnaireLogic>();
 
             
+            //postQuestionnaireLogicValidatior.re
+            var questionnaireController = new QuestionnaireController(postQuestionnaireLogicValidatior.Object);
+
+            // Act
+            ActionResult<long> result = await questionnaireController.CreateQuestionnaire(questionnaire);
+
+
+            // Assert
+            long createdQuestionnaireId = result.Value;
+            Assert.NotEqual(0, createdQuestionnaireId);
         }
     }
 }
